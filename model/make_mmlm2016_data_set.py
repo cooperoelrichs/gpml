f335a3aec2e6a8a54e4a2219e497b40b9f4fac58
cooperoelrichs <c.oelrichs@gmail.com>
2016-03-07 18:05:46 +1100

    Make teams columns instead

--- model/make_mmlm2016_data_set.py
+++ model/make_mmlm2016_data_set.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import random
@@ -17,11 +18,40 @@ def open_data_files(data_dir, file_names):
     return frames
 
 
-def calculate_regular_season_win_ratio(regular_season):
+def check_all_teams_are_accounted_for(regular_season, w_team_counts,
+                                      l_team_counts, w_team_ratios):
+    w_teams, l_teams = regular_season['Wteam'], regular_season['Lteam']
+    sorted_teams = w_teams.append(l_teams).sort_values().unique()
+
+    if (np.all([sorted_teams, w_team_counts.index.values]) and
+            np.all([sorted_teams, l_team_counts.index.values]) and
+            np.all([sorted_teams, w_team_ratios.index.values])):
+        return True
+    else:
+        return False
+
+
+def validate_w_team_ratios(regular_season, w_team_counts, l_team_counts,
+                           w_team_ratios):
+    if check_all_teams_are_accounted_for(
+            regular_season, w_team_counts, l_team_counts, w_team_ratios):
+        pass
+    else:
+        raise RuntimeError('Not all teams are accounted for!')
+
+
+def calculate_regular_season_win_ratios(regular_season):
     w_team_counts = regular_season['Wteam'].value_counts().sort_index()
     l_team_counts = regular_season['Lteam'].value_counts().sort_index()
-    w_team_ratio = w_team_counts / (w_team_counts + l_team_counts)
-    return w_team_ratio
+    w_team_ratios = w_team_counts / (w_team_counts + l_team_counts)
+    w_team_ratios = pd.DataFrame(w_team_ratios)
+
+    validate_w_team_ratios(
+        regular_season, w_team_ratios, l_team_counts, w_team_ratios)
+
+    w_team_ratios['team1'] = w_team_ratios.index
+    w_team_ratios['team2'] = w_team_ratios.index
+    return w_team_ratios
 
 
 def make_dict_for_game(series):
@@ -30,12 +60,14 @@ def make_dict_for_game(series):
     else:
         return make_results_dict(series, 'Lteam', 'Wteam', 0)
 
+
 def make_results_dict(series, team1, team2, result):
     return{
         'team1': series[team1],
         'team2': series[team2],
         'result': result
     }
+
 
 def extract_games_from_regular_season(regular_season):
     random.seed(0)
@@ -44,33 +76,6 @@ def extract_games_from_regular_season(regular_season):
         for _, series in regular_season[['Wteam', 'Lteam']].iterrows()
     ]
     return pd.DataFrame.from_dict(games)
-
-
-def add_win_ratios_to_games(games, win_ratios):
-    print(games[0:1])
-    print(win_ratios[0:1])
-
-    result = None
-    for column in ['team1', 'team2']:
-        win_ratios.name = column
-        print(pd.concat([win_ratios], axis=1)[0:9])
-        result = pd.merge(games, pd.concat([win_ratios], axis=1),
-                          on=column, how='left')
-
-    print(result[0:9])
-
-    # team1_win_ratios = win_ratios[games['team1']]
-    # team2_win_ratios = win_ratios[games['team2']]
-    # differences = team1_win_ratios - team2_win_ratios
-    #
-    # print(games)
-    # print(team1_win_ratios)
-    #
-    # games['team1_win_ratio'] = team1_win_ratios
-    # games['team2_win_ratio'] = team2_win_ratios
-    # games['win_ratio_difference'] = differences
-
-    return games
 
 
 def make_mmlm2016_data_set():
@@ -88,9 +93,14 @@ def make_mmlm2016_data_set():
 
     data_frames = open_data_files(data_dir, file_names)
     regular_season = data_frames['regular_season_detailed_results']
-    regular_season_win_ratios = calculate_regular_season_win_ratio(regular_season)
+    win_ratios = calculate_regular_season_win_ratios(regular_season)
     games = extract_games_from_regular_season(regular_season)
-    basic_data_set = add_win_ratios_to_games(games, regular_season_win_ratios)
+
+    print(type(games))
+    print(games[0:2])
+
+    print(type(win_ratios))
+    print(win_ratios[0:2])
 
     # plt.figure()
     # regular_season_win_ratios[0:40].plot(kind='bar')
