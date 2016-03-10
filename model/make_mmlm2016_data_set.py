133aaa7f46ca0b4f9ee5391c3f3fbe5188965c3b
cooperoelrichs <c.oelrichs@gmail.com>
2016-03-10 17:27:11 +1100

    Add win ratios by year to the data set

--- model/make_mmlm2016_data_set.py
+++ model/make_mmlm2016_data_set.py
@@ -5,33 +5,22 @@ import re
 from . import configer
 
 
-def check_all_teams_are_accounted_for(regular_season, w_team_counts,
-                                      l_team_counts, w_team_ratios):
+def check_all_teams_are_accounted_for(regular_season, ratios_by_year):
     w_teams, l_teams = regular_season['Wteam'], regular_season['Lteam']
     sorted_teams = w_teams.append(l_teams).sort_values().unique()
 
-    if (
-            sorted_teams.size == w_team_counts.index.values.size and
-            sorted_teams.size == l_team_counts.index.values.size and
-            sorted_teams.size == w_team_ratios.index.values.size
-        ) and (
-            np.all([sorted_teams, w_team_counts.index.values]) and
-            np.all([sorted_teams, l_team_counts.index.values]) and
-            np.all([sorted_teams, w_team_ratios.index.values])
-        ):
+    if (sorted_teams.size == ratios_by_year.index.values.size and
+            np.all([sorted_teams, ratios_by_year.index.values])):
         return True
     else:
         return False
 
 
-def validate_w_team_ratios(regular_season, w_team_counts, l_team_counts,
-                           w_team_ratios):
-    if check_all_teams_are_accounted_for(
-            regular_season, w_team_counts, l_team_counts, w_team_ratios):
+def validate_ratios_by_year(regular_season, ratios_by_year):
+    if check_all_teams_are_accounted_for(regular_season, ratios_by_year):
         pass
     else:
-        pass
-        # raise RuntimeError('Not all teams are accounted for!')
+        raise RuntimeError('Not all teams are accounted for!')
 
 
 def calculate_regular_season_win_ratios(regular_season, years):
@@ -41,19 +30,20 @@ def calculate_regular_season_win_ratios(regular_season, years):
         regular_season_for_year = regular_season[
             regular_season['Season'] == year]
 
-        w_team_counts = regular_season_for_year['Wteam'].value_counts().sort_index()
-        l_team_counts = regular_season_for_year['Lteam'].value_counts().sort_index()
+        w_team = regular_season_for_year['Wteam']
+        l_team = regular_season_for_year['Lteam']
+        w_team_counts = w_team.value_counts().sort_index()
+        l_team_counts = l_team.value_counts().sort_index()
+
         w_team_ratios = w_team_counts / (w_team_counts + l_team_counts)
         w_team_ratios.name = 'win_ratio_%i' % year
-
-        validate_w_team_ratios(
-            regular_season_for_year, w_team_ratios,
-            l_team_counts, w_team_ratios
-        )
-
         ratios.append(w_team_ratios)
 
     ratios_by_year = pd.concat(ratios, axis=1)
+    validate_ratios_by_year(regular_season, ratios_by_year)
+
+    ratios_by_year['team1'] = ratios_by_year.index
+    ratios_by_year['team2'] = ratios_by_year.index
     return ratios_by_year
 
 
@@ -81,18 +71,21 @@ def extract_games_from_regular_season(regular_season):
     return pd.DataFrame.from_dict(games)
 
 
-def merge_win_ratios_on_team(games, win_ratios, team):
-    wr_string = 'win_ratio_%s' % team
-    win_ratios[wr_string] = win_ratios['win_ratio']
+def merge_win_ratios_on_team(games, win_ratios, team, year):
+    wr_string = 'win_ratio_%i_%s' % (year, team)
+    win_ratios[wr_string] = win_ratios['win_ratio_%i' % year]
     return games.merge(win_ratios[[team, wr_string]], on=team, how='left')
 
 
-def join_games_and_win_ratios(games, win_ratios):
-    games = merge_win_ratios_on_team(games, win_ratios, 'team1')
-    games = merge_win_ratios_on_team(games, win_ratios, 'team2')
+def join_games_and_win_ratios(games, win_ratios, years):
+    for year in years:
+        games = merge_win_ratios_on_team(games, win_ratios, 'team1', year)
+        games = merge_win_ratios_on_team(games, win_ratios, 'team2', year)
 
-    games['win_ratio_difference'] = (games['win_ratio_team1'] -
-                                     games['win_ratio_team2'])
+        games['win_ratio_%i_difference' % year] = (
+            games['win_ratio_%i_team1' % year] -
+            games['win_ratio_%i_team2' % year]
+        )
     return games
 
 
@@ -100,10 +93,10 @@ def make_mmlm2016_data_set():
     config = configer.from_json('model/config_mmlm2016.json')
 
     regular_season = config.data_frames['regular_season_detailed_results']
-    win_ratios = calculate_regular_season_win_ratios(
-        regular_season, np.unique(regular_season['Season'].values))
+    years = np.unique(regular_season['Season'].values)
+    win_ratios = calculate_regular_season_win_ratios(regular_season, years)
     games = extract_games_from_regular_season(regular_season)
-    basic_data_set = join_games_and_win_ratios(games, win_ratios)
+    basic_data_set = join_games_and_win_ratios(games, win_ratios, years)
 
     basic_data_set.to_hdf(config.basic_data_set_file_name,
                           key='table', append=False)
