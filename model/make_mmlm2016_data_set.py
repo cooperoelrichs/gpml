350fca39c26ea2c22ee3f342f92214eb32ce8d55
cooperoelrichs <c.oelrichs@gmail.com>
2016-03-10 22:52:12 +1100

    Removing NaNs

--- model/make_mmlm2016_data_set.py
+++ model/make_mmlm2016_data_set.py
@@ -5,26 +5,14 @@ import re
 from . import configer
 
 
-def check_all_teams_are_accounted_for(regular_season, ratios_by_year):
+def get_all_teams_sorted(regular_season):
     w_teams, l_teams = regular_season['Wteam'], regular_season['Lteam']
     sorted_teams = w_teams.append(l_teams).sort_values().unique()
-
-    if (sorted_teams.size == ratios_by_year.index.values.size and
-            np.all([sorted_teams, ratios_by_year.index.values])):
-        return True
-    else:
-        return False
-
-
-def validate_ratios_by_year(regular_season, ratios_by_year):
-    if check_all_teams_are_accounted_for(regular_season, ratios_by_year):
-        pass
-    else:
-        raise RuntimeError('Not all teams are accounted for!')
+    return sorted_teams
 
 
 def calculate_regular_season_win_ratios(regular_season, years):
-    ratios = []
+    ratios_by_year = {}
 
     for year in years:
         regular_season_for_year = regular_season[
@@ -37,13 +25,94 @@ def calculate_regular_season_win_ratios(regular_season, years):
 
         w_team_ratios = w_team_counts / (w_team_counts + l_team_counts)
         w_team_ratios.name = 'win_ratio_%i' % year
-        ratios.append(w_team_ratios)
+        ratios_by_year[year] = w_team_ratios
 
-    ratios_by_year = pd.concat(ratios, axis=1)
-    validate_ratios_by_year(regular_season, ratios_by_year)
+        if year == 2005:
+            print(w_team_ratios[1366])
+            print(l_team_counts[1366])
+            print(w_team_counts[1366])
+            raise('!!!')
 
-    ratios_by_year['team1'] = ratios_by_year.index
-    ratios_by_year['team2'] = ratios_by_year.index
+    all_teams = get_all_teams_sorted(regular_season)
+    ratios_by_year = fill_in_missing_teams(ratios_by_year, all_teams)
+    ratios = pd.concat(list(ratios_by_year.values()), axis=1)
+
+    ratios['team1'] = ratios.index
+    ratios['team2'] = ratios.index
+    return ratios
+
+
+def fill_in_missing_teams(ratios_by_year, all_teams):
+    all_years = sorted(list(ratios_by_year.keys()))
+
+    for year, ratios in ratios_by_year.items():
+        missing_teams = np.setdiff1d(all_teams, ratios.index.values)
+
+        if year == min(all_years):
+            for team in missing_teams:
+                for replacment_year in all_years:
+                    if team in ratios_by_year[replacment_year].index:
+                        ratios_by_year[year].set_value(
+                            team, ratios_by_year[replacment_year][team])
+                        break
+                    elif replacment_year == max(all_years):
+                        raise RuntimeError('No replacment year found.')
+                    else:
+                        pass
+        elif year == max(all_years):
+            for team in missing_teams:
+                for replacment_year in reversed(all_years):
+                    if team in ratios_by_year[replacment_year].index:
+                        ratios_by_year[year].set_value(
+                            team, ratios_by_year[replacment_year][team])
+                        break
+                    elif replacment_year == min(all_years):
+                        raise RuntimeError('No replacment year found.')
+                    else:
+                        pass
+        else:
+            for team in missing_teams:
+                for left_replacment_year in all_years:
+                    if team in ratios_by_year[left_replacment_year].index:
+                        for right_replacment_year in reversed(all_years[left_replacment_year:]):
+                            if team in ratios_by_year[right_replacment_year].index:
+                                if year == 2004 and team == 1289:
+                                    print(right_replacment_year)
+                                    print(left_replacment_year)
+
+                                ratios_by_year[year].set_value(team, (
+                                    (ratios_by_year[right_replacment_year][team] +
+                                     ratios_by_year[left_replacment_year][team]) / 2
+                                ))
+                                break
+                            elif right_replacment_year == min(all_years):
+                                raise RuntimeError('No replacment year found - hit min.')
+                            else:
+                                pass
+                        break
+                    elif left_replacment_year == max(all_years):
+                        raise RuntimeError('No replacment year found - hit max.')
+                    else:
+                        pass
+
+    # Check
+    for year, ratios in ratios_by_year.items():
+        if np.isnan(ratios.values).any():
+            print(ratios[np.isnan(ratios.values)])
+            raise RuntimeError('Still contains NaNs - %i' % year)
+        elif np.array_equal(ratios.sort_index().index.values, all_teams):
+            pass
+        else:
+            print(np.setdiff1d(all_teams, ratios.index.values))
+            raise RuntimeError('Still missing teams - %i' % year)
+
+    # years = years.sort()
+    # column_and_index_of_nans =
+    # for column_name in games.columns:
+    #     row_indicies = games[column_name].isnull()
+    #     column_and_index_of_nans[column_name] = row_indicies
+    #
+    # df.T.fillna(df.mean(axis=1)).T
     return ratios_by_year
 
 
@@ -86,20 +155,30 @@ def join_games_and_win_ratios(games, win_ratios, years):
             games['win_ratio_%i_team1' % year] -
             games['win_ratio_%i_team2' % year]
         )
+
     return games
+
+
+def which_years(regular_season):
+    return np.unique(regular_season['Season'].values)
+
+
+def check_and_save_to_hdf(df, file_name):
+    if np.isnan(df.values).any():
+        raise RuntimeError('df contains NaNs - %s' % file_name)
+    df.to_hdf(file_name, key='table', append=False)
 
 
 def make_mmlm2016_data_set():
     config = configer.from_json('model/config_mmlm2016.json')
 
     regular_season = config.data_frames['regular_season_detailed_results']
-    years = np.unique(regular_season['Season'].values)
+    years = which_years(regular_season)
     win_ratios = calculate_regular_season_win_ratios(regular_season, years)
     games = extract_games_from_regular_season(regular_season)
     basic_data_set = join_games_and_win_ratios(games, win_ratios, years)
 
-    basic_data_set.to_hdf(config.basic_data_set_file_name,
-                          key='table', append=False)
+    check_and_save_to_hdf(basic_data_set, config.basic_data_set_file_name)
 
     print('Basic Data Set example:')
     print(type(basic_data_set))
@@ -134,7 +213,8 @@ def make_games_dict(id_str):
 def make_mmlm2016_submission_set():
     config = configer.from_json('model/config_mmlm2016.json')
     regular_season = config.data_frames['regular_season_detailed_results']
-    win_ratios = calculate_regular_season_win_ratios(regular_season)
+    years = which_years(regular_season)
+    win_ratios = calculate_regular_season_win_ratios(regular_season, years)
 
     # TODO Generate this properly, Hax for now.
     ss = config.data_frames['sample_submission']
@@ -145,9 +225,10 @@ def make_mmlm2016_submission_set():
     ]
 
     submission_games = pd.DataFrame.from_dict(submission_games)
-    submission_games = join_games_and_win_ratios(submission_games, win_ratios)
-    submission_games.to_hdf(config.submission_data_set_file_name,
-                            key='table', append=False)
+    submission_games = join_games_and_win_ratios(
+        submission_games, win_ratios, years)
+    check_and_save_to_hdf(submission_games,
+                          config.submission_data_set_file_name)
 
     print('Submission Data Set example:')
     print(type(submission_games))
--- model/make_mmlm2016_model.py
+++ model/make_mmlm2016_model.py
@@ -1,24 +1,40 @@
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.cross_validation import train_test_split
 from . import configer
 
 
+def x_column_names(data_set):
+    return data_set.columns - ['result', 'team1', 'team2', 'Id']
+
+
+def get_data(config):
+    basic_data_set = pd.read_hdf(config.basic_data_set_file_name,
+                                 key='table')
+
+    column_names = x_column_names(basic_data_set)
+    X = basic_data_set[column_names].values
+    y = basic_data_set['result'].values
+    return X, y
+
+
+def make_predictions(config, lr, submission_data_set):
+    column_names = x_column_names(submission_data_set)
+    X_submission = submission_data_set[column_names].values
+
+    predictions = lr.predict_proba(X_submission)
+    return predictions
+
+
 def make_mmlm2016_model():
     # TODO:
-    # 1. Win ratio by year.
     # 2. Score difference
     # 3. Seeds or seed difference?
     # 4. Ensemble with other Kagglers shared results.
 
     config = configer.from_json('model/config_mmlm2016.json')
-    basic_data_set = pd.read_hdf(config.basic_data_set_file_name,
-                                 key='table')
-
-    X = basic_data_set[
-        ['win_ratio_team1', 'win_ratio_team2', 'win_ratio_difference']
-    ].values
-    y = basic_data_set['result'].values
+    X, y = get_data(config)
 
     # TODO Seperate out a 'final test' set from cv data
 
@@ -42,12 +58,8 @@ def make_mmlm2016_model():
 
     submission_data_set = pd.read_hdf(config.submission_data_set_file_name,
                                       key='table')
+    predictions = make_predictions(config, lr, submission_data_set)
 
-    X_submission = submission_data_set[
-        ['win_ratio_team1', 'win_ratio_team2', 'win_ratio_difference']
-    ].values
-
-    predictions = lr.predict_proba(X_submission)
     submission_data_set['Pred'] = predictions[:, 1]
     submission_data_set[['Id', 'Pred']].to_csv(
         config.data_dir + 'submission.csv', sep=',', index=False)
