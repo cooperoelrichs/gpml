ce18bdb64ddc378f18e2681b5984ea6286d3718b
cooperoelrichs <c.oelrichs@gmail.com>
2016-03-17 11:45:26 +1100

    Set up new structure

---
+++ gpml/test/__main__.py
@@ -0,0 +1,9 @@
+import unittest
+from .test_data_loader import TestDataLoader
+from .test_model_maker import TestModelMaker
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
