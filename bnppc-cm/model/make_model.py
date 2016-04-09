a88007db24673e39c5793844421bb9e9b12b1be5
cooperoelrichs <c.oelrichs@gmail.com>
2016-04-09 19:21:07 +1000

    Ready for proper Compute Engine test

--- bnppc-cm/model/make_model.py
+++ bnppc-cm/model/make_model.py
@@ -89,8 +89,8 @@ def train_and_validate(model_setup, config):
     # 4. Ensemble.
     # 5. Feature engineer?
 
-    # model = model_maker.do_grid_search(
-    #     model, model_setup.parameter_grid, X_train_local, y_train_local)
+    model = model_maker.do_grid_search(
+        model, model_setup.parameter_grid, X_train_local, y_train_local)
 
     kf_results = model_maker.kfolds_evaluation(
         X_train_local, y_train_local, model)
