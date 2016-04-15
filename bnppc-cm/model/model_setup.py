349b7a7adc3474336e761d9815215bea40428896
cooperoelrichs <c.oelrichs@gmail.com>
2016-04-15 19:19:54 +1000

    Optimise ExtraTrees and test RandomForest, also start laying out an ensemble setup class

--- bnppc-cm/model/model_setup.py
+++ bnppc-cm/model/model_setup.py
@@ -3,11 +3,12 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.svm import SVC
-from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
 from gpml.model import model_maker
 from gpml.xgboost_mod.improved_early_stopping import XGBCEarlyStoppingCV
 import xgboost as xgb
 import matplotlib.pyplot as plt
+from gpml.model.ensemble import AveragingEnsemble
 
 
 class ModelSetup(object):
@@ -254,15 +255,44 @@ class ETCModelSetup(ModelSetup):
     @staticmethod
     def basic_model():
         etc = ExtraTreesClassifier(
-            n_estimators=1200,
-            max_features=30,
+            n_estimators=1000,
+            max_features=50,
             criterion='entropy',
-            min_samples_split=2,
-            max_depth=30,
+            min_samples_split=4,
+            max_depth=35,
             min_samples_leaf=2,
+
             n_jobs=-1,
-            verbose=0
+            verbose=0,
+            random_state=2
         )
+
+        # ETC with current params and small data set
+        #  - Accuracy: 0.782, LL: nan, ROC AUC: 0.749, Avg P: 0.754,
+        #    Acc 1s: 0.951, Acc 0s: 0.241
+        # ETC with previous params and expanded data set
+        #  - Accuracy: 0.787, LL: nan, ROC AUC: 0.759, Avg P: 0.751,
+        #    Acc 1s: 0.958, Acc 0s: 0.237
+        # ETC with current params and expanded data set
+        #  - Accuracy: 0.786, LL: 0.462, ROC AUC: 0.766, Avg P: 0.758,
+        #    Acc 1s: 0.971, Acc 0s: 0.193
+        # ETC with above + categorical nans set to -1
+        #  - Accuracy: 0.787, LL: 0.460, ROC AUC: 0.768, Avg P: 0.758,
+        #    Acc 1s: 0.974, Acc 0s: 0.186
+        # ETC with normalised numerics
+        #  - Accuracy: 0.787, LL: 0.460, ROC AUC: 0.768, Avg P: 0.758,
+        #    Acc 1s: 0.975, Acc 0s: 0.185
+        # ETC with normalised numerics and numeric nans set to -1
+        #  - Accuracy: 0.786, LL: 0.461, ROC AUC: 0.767, Avg P: 0.757,
+        #    Acc 1s: 0.974, Acc 0s: 0.182
+        # ETC with normalised numerics and one-hot-encoded columns
+        #  -
+        # ETC with params from et-classifier
+        #  - Accuracy: 0.786, LL: 0.460, ROC AUC: 0.766, Avg P: 0.756,
+        #    Acc 1s: 0.965, Acc 0s: 0.214
+        # ETC with factorised and one-hot-encoded categoricals
+        #  -
+
         return etc
 
     @staticmethod
@@ -289,3 +319,103 @@ class ETCModelSetup(ModelSetup):
         model = self.basic_model()
         model.set_params(**model_dump['parameters'])
         return model, results
+
+
+class RFCModelSetup(ModelSetup):
+    """Random Forest Classifier Model Setup."""
+
+    def __init__(self, config):
+        self.name = 'ExtraTreesClassifier'
+        super().__init__(config)
+
+    @staticmethod
+    def basic_model():
+        rfc = RandomForestClassifier(
+            n_estimators=1000,
+            criterion='entropy',
+            max_depth=35,  # None,
+            min_samples_split=4,  # 2,
+            min_samples_leaf=2,  # 1,
+            min_weight_fraction_leaf=0.0,
+            max_features=50,  # 'auto',
+            max_leaf_nodes=None,
+            bootstrap=False,  # True,
+
+            # n_estimators=1000,
+            # max_features=50,
+            # criterion='entropy',
+            # min_samples_split=4,
+            # max_depth=35,
+            # min_samples_leaf=2,
+
+            n_jobs=-1,
+            verbose=0,
+            random_state=1
+        )
+
+        return rfc
+
+    @staticmethod
+    def empty_booster():
+        raise NotImplementedError()
+
+    def dump_model(self, model, results, file_name):
+        model_json = {
+            'model_type_name': type(model).__name__,
+            'parameters': model.get_params(),
+            'results': results,
+        }
+
+        self.json_dump(model_json, file_name)
+
+        results_file_name = file_name.replace('.json', '_results.json')
+        self.json_dump(results, results_file_name)
+
+    def load_model(self, file_name):
+        model_dump = self.json_load(file_name)
+        results = model_dump['results']
+        print('Loaded model results:')
+        model_maker.print_dict_as_indented_list(results)
+        model = self.basic_model()
+        model.set_params(**model_dump['parameters'])
+        return model, results
+
+
+class AvgEnsModelSetup(ModelSetup):
+    """Averaging Ensemble."""
+
+    def __init__(self, config):
+        self.name = 'AveragingEnsemble'
+        self.model_setups = [
+            LRModelSetup(config),
+            SVCModelSetup(config),
+            SGDCModelSetup(config),
+            XGBModelSetup(config),
+            ETCModelSetup(config),
+            RFCModelSetup(config)
+        ]
+        self.weights = config.weights
+        super().__init__(config)
+
+    @staticmethod
+    def basic_model(self):
+        AveragingEnsemble(
+            [ms.model for ms in self.model_setups],
+            weights
+        )
+
+    @staticmethod
+    def empty_booster():
+        raise NotImplementedError()
+
+    def dump_model(self, model, results, file_name):
+        raise NotImplementedError()
+        for model_setup, (model, results) in zip(model_setups, trained):
+            model_setup.dump_model(
+                model,
+                results,
+                config.model_dump_file_names[model_setup.name]
+            )
+
+    def load_model(self, file_name):
+        raise NotImplementedError()
