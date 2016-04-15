55beb02150e5104227f039474e51d533c0c59f28
cooperoelrichs <c.oelrichs@gmail.com>
2016-04-15 19:18:49 +1000

    Added a class for handeling model averaging and start thinking about how this should work.

---
+++ gpml/model/ensemble.py
@@ -0,0 +1,13 @@
+class AveragingEnsemble(object):
+    """Average those models."""
+
+    def __init__(self, models, weights):
+        self.models = models
+        self.weights = weights
+        pass
+
+    def fit(X, y):
+        pass
+
+    def predict_proba():
+        pass
